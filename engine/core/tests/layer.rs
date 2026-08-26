8fc733a2283f1588e4a118ec974e5bcced27129f
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2026-08-26 13:12:38 +0200

    test fix + version bump v0.7.2

--- engine/core/tests/layer.rs
+++ engine/core/tests/layer.rs
@@ -217,7 +217,7 @@ fn a_blend_mode_round_trips_through_its_wire_value_and_refuses_anything_else() {
 #[test]
 fn doc_rect_to_grid_follows_layer_transform() {
     let mut layer = Layer::new("L", 100, 100);
-    let mut tiles = layer.tiles_mut().unwrap();
+    let tiles = layer.tiles_mut().unwrap();
     tiles.grow_extent(DocRect::new(200, 10, 260, 60));
     tiles.paint_rect(DocRect::new(200, 10, 260, 60), |_, _, _| {
         Some([255, 0, 0, 255])
