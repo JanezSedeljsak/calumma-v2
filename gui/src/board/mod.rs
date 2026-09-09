06a12926d77ffe9207329e76a4282c5fcda9fbc8
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2026-09-09 18:21:51 +0200

    cleanup

--- gui/src/board/mod.rs
+++
@@ -1,9 +0,0 @@
-mod cursor;
-mod host;
-mod layout;
-#[cfg(target_os = "macos")]
-mod surface_macos;
-
-pub use cursor::ModifierState;
-pub use host::BoardHost;
-pub use layout::{board_layout, hole_in_board, BoardLayout, BoardRect};
--- gui/src/input/mod.rs
+++
@@ -1,6 +0,0 @@
-mod shortcuts;
-
-pub use shortcuts::{
-    handle_key_press_for_modifiers, handle_key_release, handle_shell_key, EditorKeyAction,
-    KeyPressModifierAction, KeyReleaseAction, Modifiers, ShellKeyAction,
-};
